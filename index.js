b16ed7a7ded776a9aa1a3b9cb9fde7ef68e690f6
Sarah Ibrahim <saraibrahim4121@gmail.com>
2022-08-01 23:52:14 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue<=400){
+    return 'This one is on me!';
+  }
+    else if(someValue>400 && someValue<2000){
+      return 'That will be twenty bucks.';
+    }
+    else if(someValue>2000 && someValue<2500){
+      return 'I will gladly take your thirty bucks.';
+    }
+    else if(someValue>2500){
+      return 'No can do.';
+    }
+  }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+return ((city =='NYC') ?'Ok, sounds good.': 'No go.');
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
 }
